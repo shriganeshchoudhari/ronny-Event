732163b890417beb2a22b53194ae23dd9fb8e8a4
shri <shriganesha0@gmail.com>
2017-08-06 12:36:04 +0530

    theme applied

---
+++ ronnyevent/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,21 @@
+$(function(){
+	
+	// solving the active menu problem
+	switch(menu) {
+	
+		case 'About Us':
+			$('#about').addClass('active');
+			break;
+		case 'Contact Us':
+			$('#contact').addClass('active');
+			break;
+		case 'Services':
+			$('#services').addClass('active');
+			break;
+		 
+		default:	
+		  $('#home').addClass('active');
+			break;		
+	}
+	
+});
